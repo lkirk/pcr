d943a0d1e4c027c9627edad5716fba02891dd0b1
lloydkirk <kirklloyd@gmail.com>
2017-09-26 04:23:22 +0000

    import match, and get an initial test w/ lib code

--- src/test/test_match.cpp
+++ src/test/test_match.cpp
@@ -1,14 +1,18 @@
-// #include "match.h"
 #include "gtest/gtest.h"
 
 #define TESTING
+extern "C" {
+#include "../match.h"
+}
 
 class MyAppTestSuite : public testing::Test
 {
-  void SetUp(){}
-  void TearDown(){}
+    void SetUp(){}
+    void TearDown(){}
 };
 
-TEST_F(MyAppTestSuite, do_the_thing) {
-  EXPECT_EQ(10, 10);
+TEST_F(MyAppTestSuite, match_n_chars_found)
+{
+    char *chars_to_match = "TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_STREQ(match_n_chars("TTGGCT", 6, chars_to_match, 29), chars_to_match);
 }

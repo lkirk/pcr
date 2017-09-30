2c9d337aa03ed38c8b0899ff7d323ebde5d3d0c8
lloydkirk <kirklloyd@gmail.com>
2017-09-30 22:14:07 +0000

    test new matching fn

--- src/test/test_match.cpp
+++ src/test/test_match.cpp
@@ -17,8 +17,14 @@ TEST_F(PcrTestSuite, match_n_chars_found)
     EXPECT_STREQ(match_n_chars("TTGGCT", 6, chars_to_match, 29), chars_to_match);
 }
 
-// TEST_F(PcrTestSuite, match_char_ratio_)
-// {
-//     char *chars_to_match = "TTGGCTGAAGTAGGTAATATGCCGGCTAG";
-//     EXPECT_TRUE(match_char_ratio("TTGGCT", chars_to_match, 29, 2));
-// }
+TEST_F(PcrTestSuite, match_char_ratio)
+{
+    char *chars_to_match = "TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_TRUE(match_char_ratio("TTGGCT", chars_to_match, 6, 2));
+}
+
+TEST_F(PcrTestSuite, match_char_ratio_false)
+{
+    char *chars_to_match = "TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_FALSE(match_char_ratio("TAGCCG", chars_to_match, 6, 2));
+}

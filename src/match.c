2a657b78bb808dd10093adc488a16d562498adca
lloydkirk <kirklloyd@gmail.com>
2017-09-30 22:12:50 +0000

    not a static fn. also use substr_len not str_len

--- src/match.c
+++ src/match.c
@@ -27,16 +27,16 @@ char
     return NULL;
 }
 
-/* static bool */
-/* match_char_ratio(char *substr, char *str, int str_len, int mm_ratio) */
-/* { */
-/*     int score = 0, i; */
-/*     for (i=0; i<str_len; ++i) */
-/* 	{ */
-/* 	    if (substr[i] == str[i]) */
-/* 		score += 1; */
-/* 	    else */
-/* 		score -= 2; */
-/* 	} */
-/*     return score >= 0; */
-/* } */
+bool
+match_char_ratio(char *substr, char *str, int substr_len, int mm_ratio)
+{
+    int score = 0, i;
+    for (i=0; i<substr_len; ++i)
+    {
+	if (substr[i] == str[i])
+	    score += 1;
+	else
+	    score -= mm_ratio;
+    }
+    return true ? score >= 0 : false;
+}
--- src/match.h
+++ src/match.h
@@ -1,11 +1,14 @@
-#include <stdbool.h>
 #ifndef _PCR_MATCH_H
+
+#include <stdbool.h>
+
 struct __match_t;
 typedef struct __match_t match_t;
 
 // first place where substr of substr_len chars matches str of str_len chars
 char *match_n_chars(char *substr, int substr_len, char *str, int str_len);
 
-/* // TRUE if there are n matches per mismatch */
-/* static bool match_char_ratio(char *substr, char *str, int str_len, int mm_ratio); */
+// TRUE if there are n matches per mismatch
+bool match_char_ratio(char *substr, char *str, int substr_len, int mm_ratio);
+
 #endif

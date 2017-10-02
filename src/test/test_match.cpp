a3bc06d0cca6bfa29ae154791c9926e4a74542af
lloydkirk <kirklloyd@gmail.com>
2017-10-02 05:21:51 +0000

    make a proper test fixture. Also use proper string types to avoid compiler warnings

--- src/test/test_match.cpp
+++ src/test/test_match.cpp
@@ -1,30 +1,46 @@
 #include "gtest/gtest.h"
 
 #define TESTING
+
+#include "htslib/faidx.h"
 extern "C" {
 #include "../match.h"
 }
 
-class PcrTestSuite : public testing::Test
+class FaidxTestFixture: public testing::Test
 {
-    void SetUp(){}
-    void TearDown(){}
+protected:
+    faidx_t *fai;
+    void SetUp(){
+	fai = fai_load("/home/lkirk/references/hg38.fa");
+    }
+    void TearDown(){
+	fai_destroy(fai);
+    }
 };
 
-TEST_F(PcrTestSuite, match_n_chars_found)
+TEST(match_n_chars, match)
 {
-    char *chars_to_match = "TTGGCTGAAGTAGGTAATATGCCGGCTAG";
-    EXPECT_STREQ(match_n_chars("TTGGCT", 6, chars_to_match, 29), chars_to_match);
+    char *chars_to_match = (char*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_STREQ(match_n_chars((char*)"TTGGCT", 6, chars_to_match, 29), chars_to_match);
 }
 
-TEST_F(PcrTestSuite, match_char_ratio)
+TEST(match_char_ratio, match)
 {
-    char *chars_to_match = "TTGGCTGAAGTAGGTAATATGCCGGCTAG";
-    EXPECT_TRUE(match_char_ratio("TTGGCT", chars_to_match, 6, 2));
+    char *chars_to_match = (char*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_TRUE(match_char_ratio((char*)"TTGGCT", chars_to_match, 6, 2));
 }
 
-TEST_F(PcrTestSuite, match_char_ratio_false)
+TEST(match_char_ratio, no_match)
 {
-    char *chars_to_match = "TTGGCTGAAGTAGGTAATATGCCGGCTAG";
-    EXPECT_FALSE(match_char_ratio("TAGCCG", chars_to_match, 6, 2));
+    char *chars_to_match = (char*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_FALSE(match_char_ratio((char*)"TAGCCG", chars_to_match, 6, 2));
+}
+
+TEST_F(FaidxTestFixture, match_fasta_fetch)
+{
+    int seq_length = 0;
+    char *seq = faidx_fetch_seq(fai, "chr2", 19991, 20040, &seq_length);
+    EXPECT_TRUE(match_char_ratio((char*)"CAGTAACTTGGGGCTCCCAC", seq, 20, 2));
+    free(seq);
 }

2b93dca7d2751c897b7767b58c75fa55f41007de
lloydkirk <kirklloyd@gmail.com>
2017-10-07 12:29:34 +0000

    update tests

--- src/test/test_match.cpp
+++ src/test/test_match.cpp
@@ -21,20 +21,20 @@ extern "C" {
 
 TEST(match_n_chars, match)
 {
-    char *chars_to_match = (char*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
-    EXPECT_STREQ(match_n_chars((char*)"TTGGCT", 6, chars_to_match, 29), chars_to_match);
+    uint8_t *chars_to_match = (uint8_t*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_EQ(match_n_chars((uint8_t*)"TTGGCT", 6, chars_to_match, 29), chars_to_match);
 }
 
 TEST(match_char_ratio, match)
 {
-    char *chars_to_match = (char*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
-    EXPECT_TRUE(match_char_ratio((char*)"TTGGCT", chars_to_match, 6, 2));
+    uint8_t *chars_to_match = (uint8_t*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_TRUE(match_char_ratio((uint8_t*)"TTGGCT", chars_to_match, 6, 2));
 }
 
 TEST(match_char_ratio, no_match)
 {
-    char *chars_to_match = (char*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
-    EXPECT_FALSE(match_char_ratio((char*)"TAGCCG", chars_to_match, 6, 2));
+    uint8_t *chars_to_match = (uint8_t*)"TTGGCTGAAGTAGGTAATATGCCGGCTAG";
+    EXPECT_FALSE(match_char_ratio((uint8_t*)"TAGCCG", chars_to_match, 6, 2));
 }
 
 // TEST_F(FaidxTestFixture, match_fasta_fetch)
